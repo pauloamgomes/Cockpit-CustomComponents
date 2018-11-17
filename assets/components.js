0e7cf922a3b0071b71dedc59eae8d810e2b92801
Paulo Gomes <pauloamgomes@gmail.com>
2018-11-17 00:47:57 +0000

    validation

--- assets/components.js
+++ assets/components.js
@@ -1,6 +1,6 @@
-App.on('field.layout.components', function(field) {
+App.on("field.layout.components", function(field) {
   window.setTimeout(function() {
-    if (field.params.opts.enabled !== undefined) {
+    if (field.params.opts && field.params.opts.enabled !== undefined) {
       Object.keys(field.params.components).forEach(function(key) {
         if (!field.params.opts.enabled.includes(key)) {
           delete field.params.components[key];
