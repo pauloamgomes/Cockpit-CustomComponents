5dfbce3d133e042c0fa41e72367592e75c1a51c9
Alexander Jank <himself@alexanderjank.de>
2019-01-28 18:52:43 +0100

    feat: Re-enable component disabling from config.yaml; Disable components through the `disabled` opti

--- assets/components.js
+++ assets/components.js
@@ -1,8 +1,22 @@
-App.on("field.layout.components", function(field) {
+App.on('field.layout.components', function(field) {
+  App.callmodule('customcomponents:getDisabled').then(function(data) {
+      if (data && data.result) {
+          data.result.forEach(function(component) {
+            delete field.params.components[component];
+          });
+      }
+  });
   window.setTimeout(function() {
     if (field.params.opts && field.params.opts.enabled !== undefined) {
       Object.keys(field.params.components).forEach(function(key) {
         if (!field.params.opts.enabled.includes(key)) {
+          delete field.params.components[key];
+        }
+      });
+    }
+    if (field.params.opts && field.params.opts.disabled !== undefined) {
+      Object.keys(field.params.components).forEach(function(key) {
+        if (field.params.opts.disabled.includes(key)) {
           delete field.params.components[key];
         }
       });
