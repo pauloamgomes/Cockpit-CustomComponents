27a495dd246407b9346fec803294a8b9386042ed
Peter Larsson <peter@barschool.net>
2019-04-25 16:55:04 +0700

    Adding support for layout components Adding support for disabled

--- assets/components.js
+++ assets/components.js
@@ -1,11 +1,27 @@
 App.on("field.layout.components", function(field) {
+  function handleComponents(op, which) {
+    Object.keys(field.params.components).forEach(function(key) {
+      if ((op === "enable" && !which.includes(key)) || (op === "disable" && which.includes(key))) {
+        delete field.params.components[key]
+      }
+    })
+  }
+
   window.setTimeout(function() {
     if (field.params.opts && field.params.opts.enabled !== undefined) {
-      Object.keys(field.params.components).forEach(function(key) {
-        if (!field.params.opts.enabled.includes(key)) {
-          delete field.params.components[key];
-        }
-      });
+      handleComponents("enable", field.params.opts.enabled)
     }
-  }, 100);
-});
+
+    if (field.params.opts.parent && field.params.opts.parent.enabled !== undefined) {
+      handleComponents("enable", field.params.opts.parent.enabled)
+    }
+
+    if (field.params.opts && field.params.opts.disabled !== undefined) {
+      handleComponents("disable", field.params.opts.disabled)
+    }
+
+    if (field.params.opts.parent && field.params.opts.parent.disabled !== undefined) {
+      handleComponents("disable", field.params.opts.parent.disabled)
+    }
+  }, 100)
+})
